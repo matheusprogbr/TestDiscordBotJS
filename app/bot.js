7d1a0343cbcb3d9063bc7226b744f0265118f031
Matheus Henrique <matheusprogbr@gmail.com>
2020-12-08 01:49:21 -0300

    feat: Add some random and simple features, just for testing this bot

--- app/bot.js
+++ app/bot.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const docReply = [
+  'It looks like you said DOCUMENTATION!?!?!?!',
+  'You might find something useful in these links:',
+  'https://discordjs.guide/',
+];
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', (msg) => {
+  if (msg.content.includes('documentation')) {
+    msg.reply(docReply);
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
